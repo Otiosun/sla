0c7e6034bd75bb2f2ceb8b71f8984cfb8b6b7791
Otiosun <natanaelgames19@gmail.com>
2026-08-27 01:45:17 -0300

    feat: add pokemon effect admin owner port

---
+++ src/modules/pokemon/effect-admin-ports.ts
@@ -0,0 +1,10 @@
+import type {
+  ApplyPokemonEffectInput,
+  RemovePokemonEffectInput,
+} from "./admin-contracts.js";
+import type { PokemonAdminPersistenceResult } from "./admin-ports.js";
+
+export interface PokemonEffectAdminRepository {
+  applyEffect(input: ApplyPokemonEffectInput): Promise<PokemonAdminPersistenceResult>;
+  removeEffect(input: RemovePokemonEffectInput): Promise<PokemonAdminPersistenceResult>;
+}
