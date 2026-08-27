bb80be86e4e409095d5c457c1a015206c3f54b6e
Otiosun <natanaelgames19@gmail.com>
2026-08-27 13:18:44 -0300

    wip: add Pokemon lifecycle admin port

---
+++ src/modules/pokemon/lifecycle-admin-ports.ts
@@ -0,0 +1,10 @@
+import type {
+  CorrectPokemonProgressionInput,
+  CreatePokemonInput,
+} from "./admin-contracts.js";
+import type { PokemonAdminPersistenceResult } from "./admin-ports.js";
+
+export interface PokemonLifecycleAdminRepository {
+  createPokemon(input: CreatePokemonInput): Promise<PokemonAdminPersistenceResult>;
+  correctProgression(input: CorrectPokemonProgressionInput): Promise<PokemonAdminPersistenceResult>;
+}
