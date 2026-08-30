a1cf52744cc17c88d9ba1b926c62bc6019ba2157
Otiosun <natanaelgames19@gmail.com>
2026-08-29 22:03:26 -0300

    Format post-deploy smoke CLI

--- scripts/operations/post-deploy-application-smoke.ts
+++ scripts/operations/post-deploy-application-smoke.ts
@@ -1,4 +1,7 @@
-import { createDatabasePool, closeDatabasePool } from "../../src/platform/db/database.js";
+import {
+  createDatabasePool,
+  closeDatabasePool,
+} from "../../src/platform/db/database.js";
 import { loadConfig } from "../../src/platform/config/env.js";
 import { runPostDeployApplicationSmoke } from "../../src/operations/post-deploy-application-smoke.js";
 
