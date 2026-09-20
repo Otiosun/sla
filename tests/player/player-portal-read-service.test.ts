e359f9a9a882d4e53ca4110de4f5a9ef7f41c1a7
Otiosun <natanaelgames19@gmail.com>
2026-09-20 20:41:18 -0300

    Align Hub form slug fixtures with catalog identity

--- tests/player/player-portal-read-service.test.ts
+++ tests/player/player-portal-read-service.test.ts
@@ -69,7 +69,7 @@ function service(activeBattle: PlayerPortalActiveBattleRecord | null = null) {
         {
           pokemonInstanceId,
           formId: "66666666-6666-4666-8666-666666666666",
-          formSlug: "default",
+          formSlug: "chikorita",
           speciesSlug: "chikorita",
           displayName: "Chikorita",
           nationalDex: 152,
@@ -117,7 +117,7 @@ describe("PlayerPortalReadService", () => {
       team: [
         {
           pokemonInstanceId,
-          formSlug: "default",
+          formSlug: "chikorita",
           speciesSlug: "chikorita",
           displayName: "Chikorita",
           currentHp: 20,
