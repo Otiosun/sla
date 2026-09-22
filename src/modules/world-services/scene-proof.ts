b494f698229c3eb6c4376e73329c9fc664565dea
Otiosun <natanaelgames19@gmail.com>
2026-09-22 16:42:09 -0300

    style: satisfy scene proof lint

--- src/modules/world-services/scene-proof.ts
+++ src/modules/world-services/scene-proof.ts
@@ -1,6 +1,6 @@
 export const MIN_SCENE_PROOF_WORDS = 50;
 
-const WORD_PATTERN = /[\p{L}\p{N}]+(?:['’\-][\p{L}\p{N}]+)*/gu;
+const WORD_PATTERN = /[\p{L}\p{N}]+(?:['’-][\p{L}\p{N}]+)*/gu;
 const COMMAND_TOKEN_PATTERN = /\/[\p{L}\p{N}_-]+/gu;
 
 export function sceneProofWordCount(text: string): number {
